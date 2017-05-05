d806d4a6344465ac3ddac5f1555d88baf23326ce
Assad Aboultaif <assadaboultaif@gmail.com>
2017-05-05 14:47:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Project Overview
+
+# Feed Reader Testing Project
+
+This project is part of Udacity's Front End Development nanodegree program.
+
+In this project I write a number of tests against a pre-existing application. These will test the underlying business logic of the application as well as the event handling and DOM manipulation. The application given is a web-based application that reads RSS feeds. 
+
+* Writing effective tests requires analyzing multiple aspects of an application including the HTML, CSS and JavaScript - an extremely important skill when changing teams or joining a new company.
+* Good tests give you the ability to quickly analyze whether new code breaks an existing feature within your codebase, without having to manually test all of the functionality.
+
+
+## Why this Project?
+
+Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+
+Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
+
+## Project Implementation
+
+Used Jasmine's behavior-driven development framework for testing Javascript code. For more information, review the [Jasmine documentation](http://jasmine.github.io).
+
+* Write a test that loops through each feed in the `allFeeds` object and ensures it has a URL defined and that the URL is not empty.
+* Write a test that loops through each feed in the `allFeeds` object and ensures it has a name defined and that the name is not empty.
+* Write a new test suite named `"The menu"`.
+* Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
+* Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
+* Write a test suite named `"Initial Entries"`.
+* Write a test that ensures when the `loadFeed` function is called and completes its work, there is at least a single `.entry` element within the `.feed` container.
+* Write a test suite named `"New Feed Selection"`.
+* Write a test that ensures when a new feed is loaded by the `loadFeed` function that the content actually changes.
+
+
+
+Review the Feed Reader Testing [Project Rubric](https://review.udacity.com/#!/projects/3442558598/rubric)
+
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,106 @@
+// feedreader.js
+
+// This test suites specification will test app.js
+
+$(function() {
+ 
+
+    var totalFeedLength =  allFeeds.length;
+
+    
+    // Will test RSS feeds definitions and variables
+    describe('RSS Feeds', function() {
+
+
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(totalFeedLength).not.toBe(0);
+        });
+
+
+        // Test makes sure there is a URL defined and is not empty
+         it('URL defined and not empty',function(){
+            for(var i=0; i< totalFeedLength; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+         });
+
+
+         // Test makes sure tehere is a name defined and that the name is not empty
+         it('Name is defined and not empty',function(){
+            for(var i=0 ; i<totalFeedLength; i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+
+         });
+    });
+
+    // Will test Menu functionality 
+    describe('The menu',function(){
+        
+       
+         // Test checks if element hidden by default
+         it('Menu hidden by default',function(){
+            expect($("body").hasClass("menu-hidden")).toBe(true);
+         });
+
+         //Test checks if element's visibility changes when clicked
+
+         it('Menu changes visibility when menu is clicked',function(){
+            //Checks if menu opens
+            $(".menu-icon-link").click();
+            expect($("body").hasClass("menu-hidden")).toBe(false);
+
+            //Checks if menu closes when clicked again
+            $(".menu-icon-link").click();
+            expect($("body").hasClass("menu-hidden")).toBe(false);
+         });
+
+
+    });
+
+     // Test suite checks for initial entries  
+    describe('Initial Entries',function(){
+
+        beforeEach(function(done){
+            loadFeed(0,done);
+        });
+
+        /*ensures when the loadFeed function is called and completes its work, there is at least
+         a single .entry element within the .feed container */
+
+        it('Should have at least a single .entry element within the .feed container',function(){
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+
+    });
+
+    // Test suite for new feed selection
+    describe('New Feed Selection'function(){
+
+        /* Write a test that ensures when a new feed is loaded
+         by the loadFeed function that the content actually changes.
+        var entry1,entry2 */
+
+        beforeEach(function(done){
+            loadFeed(1,function){
+                result1 = $(".feed").html();
+                done();
+            }
+
+        });
+
+        it('New feed is loaded',function(done){
+            loadFeed(0,function(){
+                result2= $(".feed").html();
+                expect(result1).not.toEqual(result2);
+                done();
+            });
+
+        });
+
+    });
+
+}());
